142232066aaa5ce16b5110ac0c651f455d165e18
Eeli Haapalainen <eeli.haapalainen@helsinki.fi>
2025-11-03 19:23:25 +0200

    v1/ns: refactor PlayerReader and StatisticsService to take sources in constructor

--- viikko1/nhl-statistics/src/index.py
+++ viikko1/nhl-statistics/src/index.py
@@ -1,8 +1,10 @@
+from player_reader import PlayerReader
 from statistics_service import StatisticsService
 
 
 def main():
-    stats = StatisticsService()
+    reader = PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt")
+    stats = StatisticsService(reader)
     philadelphia_flyers_players = stats.team("PHI")
     top_scorers = stats.top(10)
 
