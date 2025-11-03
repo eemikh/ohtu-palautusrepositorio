bf920171ae28bc8cd83e3af9bcc712126a6f7a06
Eeli Haapalainen <eeli.haapalainen@helsinki.fi>
2025-11-03 19:34:23 +0200

    v1/ns/tests: full coverage for StatisticsService

---
+++ viikko1/nhl-statistics/src/tests/statistics_service_test.py
@@ -0,0 +1,38 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),  #  4+12 = 16
+            Player("Lemieux", "PIT", 45, 54), # 45+54 = 99
+            Player("Kurri",   "EDM", 37, 53), # 37+53 = 90
+            Player("Yzerman", "DET", 42, 56), # 42+56 = 98
+            Player("Gretzky", "EDM", 35, 89)  # 35+89 = 124
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_search_found(self):
+        self.assertEqual(self.stats.search("Yzerman").name, "Yzerman")
+
+    def test_search_not_found(self):
+        self.assertIsNone(self.stats.search("Me"))
+
+    def test_team(self):
+        edm = self.stats.team("EDM")
+        edm_player_names = sorted([player.name for player in edm])
+
+        self.assertEqual(edm_player_names, ["Gretzky", "Kurri", "Semenko"])
+
+    def test_top(self):
+        top = self.stats.top(2)
+        top_player_names = [player.name for player in top]
+
+        self.assertEqual(top_player_names, ["Gretzky", "Lemieux", "Yzerman"])
