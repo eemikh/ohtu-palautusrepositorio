935367a54caf23a79e236309cb440d59654df6d5
Eeli Haapalainen <eeli.haapalainen@helsinki.fi>
2025-11-10 18:08:16 +0200

    v1/nr: print finnish players' stats

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,22 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    finnish_players = [player for player in players if player.nationality == "FIN"]
+
+    print("Suomalaiset pelaajat:")
+
+    for player in finnish_players:
+        print(f"{player.name} team {player.team}, {player.goals} goals, {player.assists} assists")
 
 if __name__ == "__main__":
     main()
